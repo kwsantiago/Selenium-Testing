3b42bf201ffe8f8937b667965f85a986fd30395d
Kyle <44934418+kwsantiago@users.noreply.github.com>
2023-04-11 23:31:24 -0400

    add comments

--- test_suite.py
+++ test_suite.py
@@ -1,20 +1,20 @@
 import unittest
 
-# import your test-case files here
+# Import test-case files
 from tc1_valid_input import TestValidInput
 from tc2_empty_fields import TestEmptyFields
 from tc3_invalid_email import TestInvalidEmail
 from tc4_password_mismatch import TestPasswordMismatch
 
-# create a TestSuite object
+# Create a TestSuite object
 test_suite = unittest.TestSuite()
 
-# add each test case to the suite
+# Add each test case to the suite
 test_suite.addTest(unittest.makeSuite(TestValidInput))
 test_suite.addTest(unittest.makeSuite(TestEmptyFields))
 test_suite.addTest(unittest.makeSuite(TestInvalidEmail))
 test_suite.addTest(unittest.makeSuite(TestPasswordMismatch))
 
-# run the test suite
+# Run the test suite
 test_runner = unittest.TextTestRunner(verbosity=2)
 test_runner.run(test_suite)
